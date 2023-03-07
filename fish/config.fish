5659ded5ed630d3fdf42910726baee352154c628
yuzhegan <boflyyang@163.com>
2023-03-07 16:36:28 +0800

    C-v C-x lf config

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set fish_greeting ""
 export EDITOR=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
+export LC_CTYPE=zh_CN.UTF-8
+export LANG=zh_CN.UTF-8
 # set -x DISPLAY (awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
 set TERM xterm-256color
 [ -z "awk (ps -ef | grep cron | grep -v grep)" ] && sudo /etc/init.d/cron start &> /dev/null
